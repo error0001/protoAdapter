d14d353591b997be9303553d40f07098b699cc0c
error0001 <error00010001@gmail.com>
2018-10-16 16:44:31 +0300

    new file cur

--- protoAdapter/curAdapt.h
+++ protoAdapter/curAdapt.h
@@ -1,21 +1,48 @@
 #pragma once
+#ifndef CURADAPT_H
+#include <iostream>
+using namespace std;
+#endif
 
 class BaseABC
 {
 public: 
+	BaseABC()
+	{
+	}
+	virtual void Init() = 0;
+	virtual void GetDataA() = 0;
+	virtual void GetDataB() = 0; 
 };
 
 class WrapperABC : public BaseABC, private ItemA, private ItemB
 {
+public:
+	WrapperABC(BaseABC someABC)
+	{
 
+	}
+	void Init(){}
 };
 
-class ItemA
+class ItemA : public BaseABC
 {
-
+public:
+	ItemA()
+	{
+		cout << "Create item A" << endl;
+	}
+	void Init(){}
+	void GetDataA(){}
 };
 
-class ItemB
+class ItemB : public BaseABC
 {
-
+public:
+	ItemB()
+	{
+		cout << "Create item A" << endl;
+	}
+	void Init(){}
+	void GetDataB(){}
 };
